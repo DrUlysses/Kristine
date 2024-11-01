e95be29c455e10d3f312a167847697dfac6c4b90
Ulysses <drulysses@users.noreply.github.com>
2024-11-01 10:12:05 +0100

    Android build fix

--- composeApp/src/androidMain/kotlin/dr/ulysses/database/Database.android.kt
+++ composeApp/src/androidMain/kotlin/dr/ulysses/database/Database.android.kt
@@ -1,12 +1,13 @@
 package dr.ulysses.database
 
 import android.content.Context
+import app.cash.sqldelight.async.coroutines.synchronous
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.android.AndroidSqliteDriver
 
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 actual class DriverFactory(private val context: Context) {
     actual suspend fun createDriver(name: String): SqlDriver {
-        return AndroidSqliteDriver(Database.Schema, context, name)
+        return AndroidSqliteDriver(Database.Schema.synchronous(), context, name)
     }
 }
