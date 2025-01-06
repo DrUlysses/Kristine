881656d4a90e881d8149aacff4ce78818eb71410
DrUlysses <drulysses@users.noreply.github.com>
2025-01-06 19:39:46 +0100

    Disabled io for wasm

--- composeApp/src/wasmJsMain/kotlin/dr/ulysses/entities/Song.wasmJs.kt
+++ composeApp/src/wasmJsMain/kotlin/dr/ulysses/entities/Song.wasmJs.kt
@@ -1,23 +1,20 @@
 package dr.ulysses.entities
 
-import kotlinx.io.files.Path
-import kotlinx.io.files.SystemFileSystem
-
 actual suspend fun refreshSongs(): List<Song> {
-    val songsRootPath = Path(SettingsRepository.get("songsPath")?.value ?: ".")
-    val songs = SystemFileSystem.list(songsRootPath).filter {
-        it.name.substringBeforeLast(".") in listOf("mp3", "flac", "wav", "ogg", "m4a", "wma", "aac")
-    }.map {
-        Song(
-            path = "file:///$it",
-            title = it.name,
-            artist = "Unknown",
-        )
-    }
+//    val songsRootPath = Path(SettingsRepository.get("songsPath")?.value ?: ".")
+//    val songs = SystemFileSystem.list(songsRootPath).filter {
+//        it.name.substringBeforeLast(".") in listOf("mp3", "flac", "wav", "ogg", "m4a", "wma", "aac")
+//    }.map {
+//        Song(
+//            path = "file:///$it",
+//            title = it.name,
+//            artist = "Unknown",
+//        )
+//    }
+//
+//    songs.forEach {
+//        SongRepository.upsert(it)
+//    }
 
-    songs.forEach {
-        SongRepository.upsert(it)
-    }
-
-    return songs
+    return emptyList() // Disable for now (coz file system is not available in wasm)
 }
